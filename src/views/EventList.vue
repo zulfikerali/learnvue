504db3534ea07027ba5f0dd2405f588de188c49f
zulfikerali <zulfiker.phy@gmail.com>
2021-08-21 14:35:57 +0600

    db

---
+++ src/views/EventList.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Events for good!</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
